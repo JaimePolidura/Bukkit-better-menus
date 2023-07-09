f5769f3a0fa7a2d164a7905b55467bfd181203d4
Jaime Polidura <jaime.polidura@gmail.com>
2023-07-09 12:41:55 +0200

    Added MenuTimerRunnable on MenuTimer initialization to constructor

--- src/main/java/es/bukkitbettermenus/modules/timers/MenuTimer.java
+++ src/main/java/es/bukkitbettermenus/modules/timers/MenuTimer.java
@@ -7,19 +7,23 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.function.BiConsumer;
 
-@RequiredArgsConstructor
 public final class MenuTimer {
     private final BiConsumer<BukkitRunnable, Integer> onTick;
     private final TimerExecutionType executionType;
     private final long runEveryTick;
-
+    private final BukkitRunnable runnable;
+    
     private int timesCalled;
 
-    private BukkitRunnable runnable;
+    public MenuTimer(BiConsumer<BukkitRunnable, Integer> onTick, TimerExecutionType executionType, long runEveryTick) {
+        this.onTick = onTick;
+        this.executionType = executionType;
+        this.runEveryTick = runEveryTick;
+        this.runnable = new MenuTimerRunnable();
+        this.timesCalled = 0;
+    }
 
     public void start() {
-        this.runnable = new MenuTimerRunnable();
-
         Bukkit.getScheduler().runTaskTimer(
                 BukkitBetterMenus.PLUGIN,
                 runnable,
